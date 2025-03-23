4e8bf9bde26a42f63771e34b3dfb4598917b0e8e
vikaIVANO <dimakolt86@gmail.com>
2025-03-23 17:48:29 +0300

    Добавлен класс Product

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Product
+{
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public float Price { get; set; }
+
+    public Product(string name, int quantity, float price)
+    {
+        Name = name;
+        Quantity = quantity;
+        Price = price;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Товар: {Name}, Количество: {Quantity}, Стоимость: {Price}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Product product = new Product("Молоко", 10, 50.5f);
+        product.DisplayInfo();
+    }
+}
